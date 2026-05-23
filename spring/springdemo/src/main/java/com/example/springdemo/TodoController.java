e78cebaf21342fa380dc77d3a96765afc9d68147
Sudarmani <bsudarmani2006@gmail.com>
2026-05-23 23:33:35 +0530

    .spring

---
+++ spring/springdemo/src/main/java/com/example/springdemo/TodoController.java
@@ -0,0 +1,32 @@
+package com.example.springdemo;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class TodoController
+{
+    //get mapping
+    @GetMapping("/get")
+    public String getTodo()
+    {
+        return "gettodo";
+    }
+    //path variable
+    @GetMapping("/{id}")
+    public String getTodoid(@PathVariable int id)
+    {
+        return "path variable id"+id;
+    }
+
+    //request param
+    @GetMapping("")
+    public String getTodoParam(@RequestParam("paramid") long id)
+    {
+        return "request param id"+id;
+    }
+
+    //postmapping
+
+}
